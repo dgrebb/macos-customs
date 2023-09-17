528b746c20be816e7b8ed7c88eae4aabb70d971e
Dan Grebb <dan@dgrebb.com>
2023-09-17 18:30:57 -0400

    build(config): adds apple music plugin to sketchybar

---
+++ .config/sketchybar/items/music.sh
@@ -0,0 +1,19 @@
+# Add event
+sketchybar -m --add event song_update com.apple.iTunes.playerInfo
+
+# Add Music Item
+sketchybar -m --add item music center \
+  --set music script="$PLUGIN_DIR/music.sh" \
+  click_script="$PLUGIN_DIR/music_click.scpt" \
+  label.padding_right=10 \
+  icon.padding_left=10 \
+  drawing=off \
+  --subscribe music song_update
+
+music_bracket=(
+  background.color=$BACKGROUND_1
+  background.border_color=$BACKGROUND_2
+)
+
+sketchybar --add bracket musicb music \
+  --set musicb "${music_bracket[@]}"
