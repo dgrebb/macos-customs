4fb24e27e80417d0af224b346df048f0402d60f0
Dan Grebb <dan@dgrebb.com>
2023-09-23 20:07:55 -0400

    feat(sketchybar): adds sketchybar network plugin

---
+++ .config/sketchybar/items/network.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sketchybar --add item ip_address right \
+  --set ip_address script="~/.config/sketchybar/plugins/ip_address.sh" \
+  update_freq=30 \
+  padding_left=2 \
+  padding_right=8 \
+  background.border_width=0 \
+  background.corner_radius=6 \
+  background.height=24 \
+  icon.highlight=off \
+  label.highlight=off \
+  --subscribe ip_address wifi_change
+
+sketchybar --add item network.up right \
+  --set network.up script="~/.config/sketchybar/plugins/network.sh" \
+  update_freq=10 \
+  padding_left=2 \
+  padding_right=2 \
+  background.border_width=0 \
+  background.height=24 \
+  icon=⇡ \
+  icon.color=$YELLOW \
+  label.color=$YELLOW
+
+sketchybar --add item network.down right \
+  --set network.down script="~/.config/sketchybar/plugins/network.sh" \
+  update_freq=10 \
+  padding_left=8 \
+  padding_right=2 \
+  background.border_width=0 \
+  background.height=24 \
+  icon=⇣ \
+  icon.color=$GREEN \
+  label.color=$GREEN
+
+# Bracket
+
+sketchybar --add bracket status ip_address network.up network.down \
+  --set status background.color=$BACKGROUND_1 \
+  background.border_color=$BACKGROUND_2
