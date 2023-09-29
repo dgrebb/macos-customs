978b54188dc29e84bc570dac9601edf7566f3e18
Dan Grebb <Daniel_Grebb@comcast.com>
2023-09-29 14:00:47 -0400

    feat(sketchybar): adds function to handle space navigation for sip-enabled machines

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+switchdesktop() {
+  typeset -A desktophash
+  desktophash[0]=29
+  desktophash[1]=18
+  desktophash[2]=19
+  desktophash[3]=20
+  desktophash[4]=21
+  desktophash[5]=23
+  desktophash[6]=22
+  desktophash[7]=26
+  desktophash[8]=28
+  desktophash[9]=25
+  desktopkey=${desktophash[$1]}
+  osascript -e "tell application \"System Events\" to key code $desktopkey using control down"
+}
+
 update() {
   source "$CONFIG_DIR/colors.sh"
   BG_COLOR=$BACKGROUND_1
@@ -19,7 +35,7 @@ mouse_clicked() {
     yabai -m space --destroy $SID
     sketchybar --trigger windows_on_spaces --trigger space_change
   else
-    yabai -m space --focus $SID 2>/dev/null
+    yabai -m space --focus $SID 2>/dev/null || switchdesktop $SID
   fi
 }
 
