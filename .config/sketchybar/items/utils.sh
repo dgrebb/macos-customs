6cedad2aef9aa506a9f3566e465f5227755c8953
Dan Grebb <dan@dgrebb.com>
2023-10-03 18:28:55 -0400

    feat(sketchybar): adds conditional utils based on installed/running

--- .config/sketchybar/items/utils.sh
+++ .config/sketchybar/items/utils.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
 utils_bracket=(
   background.color=$BACKGROUND_1
   background.border_color=$BACKGROUND_2
   associated_display=1
 )
 
-sketchybar --add bracket utils volume_icon omnifocus teams "Shifty,Item-0" "iStat Menus Status,com.bjango.istatmenus.memory" "iStat Menus Status,com.bjango.istatmenus.cpu" \
+# Set up machine-specific apps
+utils=()
+TEAMS=$(lsappinfo info "Microsoft Teams")
+
+if [[ $TEAMS ]]; then
+  utils+=teams
+  source "$ITEM_DIR/teams.sh"
+else
+  echo "Skipping Teams util as it is not installed."
+fi
+
+sketchybar --add bracket utils volume_icon omnifocus $utils "Shifty,Item-0" "iStat Menus Status,com.bjango.istatmenus.memory" "iStat Menus Status,com.bjango.istatmenus.cpu" \
   --set utils "${utils_bracket[@]}" \
   associated_display=1
