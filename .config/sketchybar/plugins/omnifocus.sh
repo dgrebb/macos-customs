6b40041e6010fb3adf64a97fc031838b82614f63
Dan Grebb <dan@dgrebb.com>
2023-10-07 21:57:21 -0400

    style(sketchybar): updates omnifocus states

--- .config/sketchybar/plugins/omnifocus.sh
+++ .config/sketchybar/plugins/omnifocus.sh
@@ -4,6 +4,7 @@ source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "OmniFocus")
 ICON=""
+LABEL_PADDING=3
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
   LABEL="${BASH_REMATCH[1]}"
 
@@ -12,13 +13,16 @@ if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
   elif [[ $LABEL == "•" ]]; then
     ICON_COLOR="0xffeed49f"
   elif [[ $LABEL =~ ^[0-9]+$ ]]; then
+    ICON=""
     ICON_COLOR="$PURPLE"
   else
     exit 0
   fi
 else
-  LABEL="0"
+  ICON=""
+  LABEL=""
+  LABEL_PADDING=0
   ICON_COLOR="$GREY"
 fi
 
-sketchybar --set $NAME icon=$ICON label="${LABEL}" icon.color=${ICON_COLOR}
+sketchybar --set $NAME icon=$ICON label="${LABEL}" icon.color=${ICON_COLOR} icon.padding_right="$LABEL_PADDING" icon.padding_left="$LABEL_PADDING" label.padding_left="$LABEL_PADDING" label.padding_right="$LABEL_PADDING"
