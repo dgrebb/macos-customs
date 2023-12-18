bdb4f7e07d44d74907910b9d279190af8d5c9c31
Dan Grebb <dan@dgrebb.com>
2023-12-18 10:08:05 -0500

    build(sketchybar): updates teams item configuration

--- .config/sketchybar/items/teams.sh
+++ .config/sketchybar/items/teams.sh
@@ -1,17 +1,20 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+teams=(
+  script="$PLUGIN_DIR/teams.sh"
+  click_script="$PLUGIN_DIR/teams_click.sh"
+  updates=on
+  update_freq=30
+  label.y_offset=-1
+  icon.y_offset=1
+  icon.font.size=18
+  icon.padding_left=3
+  icon.padding_right=0
+  label.padding_left=3
+  label.padding_right=3
+  associated_display=1
+)
 
 sketchybar --add item teams right \
-  --set teams \
-  update_freq=30 \
-  script="$PLUGIN_DIR/teams.sh" \
-  label.y_offset=-1 \
-  icon.y_offset=1 \
-  icon.font.size=18 \
-  icon.padding_left=3 \
-  icon.padding_right=0 \
-  label.padding_left=3 \
-  label.padding_right=3 \
-  click_script="$PLUGIN_DIR/teams_click.sh" \
-  associated_display=1
-
-sketchybar --subscribe teams system_woke
+  --set teams "${teams[@]}" \
+  --subscribe teams system_woke
