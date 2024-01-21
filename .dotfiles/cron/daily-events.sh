b3a7340c74851d1c5cf5c1a800aa40cf97120ef4
Dan Grebb <dan@dgrebb.com>
2024-01-20 22:24:15 -0500

    wip: add daily-events script

---
+++ .dotfiles/cron/daily-events.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Settings
+# VAULT_PATH="/Users/dgrebb/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Office"
+# TEMPLATE_PATH="$VAULT_PATH/_Logs/_Templates"
+
+# # Get Date
+# DATE=$()
+
+# # Copy Template
+# TEMPLATE=$(cat $TEMPLATE_PATH/_Daily.md)
+
+# Get Tomorrow's Activities from Apple Calendar
+TEXT=$(icalBuddy -f -n -sc -ss "" -b "- [ ] " -ab "- [ ] ‼️ " -iep title,datetime -ps "| ⤇ |" -po "datetime,title" -tf "%H:%M" -df "" eventsFrom:"tomorrow" to:"tomorrow")
+
+# Print that list to the "# Today" section of tomorrow's Daily Obsidian note
+# echo "# Today's Activities" >>
+echo "$TEXT"
