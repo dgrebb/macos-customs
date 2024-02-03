e5be016b914f72a27c6b0659a2fbe8e0f97556f2
Dan Grebb <git@dgrebb.com>
2024-02-02 22:58:08 -0500

    chore: remove vscode extensions script and list

--- .vscode/vscode-extensions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# install vscode extensions from exported list
-# https://github.com/microsoft/vscode/issues/42994
-
-# NOTE: the below will ignore commented lines in the list
-cat extensions.list | grep -v '^#' | xargs -L1 code --install-extension
