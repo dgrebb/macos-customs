548e3449fb5c10cda583e983da322d489871f959
Dan Grebb <git@dgrebb.com>
2024-02-03 04:09:09 -0500

    chore: combine zsh setup scripts

--- steps/zsh.sh
+++ steps/zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh -c "$$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
 # Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
