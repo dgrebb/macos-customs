2e6921bf314e45114f107f16045e5113e08a88bf
Dan Grebb <git@dgrebb.com>
2024-02-12 20:42:47 -0500

    chore: low light wallpapers

--- .config/sketchybar/plugins/wallpaper.sh
+++ .config/sketchybar/plugins/wallpaper.sh
@@ -16,7 +16,7 @@ source "$HOME/.config/sketchybar/colors.sh"
 source "$HOME/.config/.secrets"
 
 # Update
-QUERY="fruit macro"
+QUERY="dark beautiful"
 ORIENTATION="landscape"
 TIMESTAMP=$(echo '('$(date +"%s.%N") ' * 10)/1' | bc)
 WALLPAPER_PATH="$HOME/Pictures/wallpaper"
